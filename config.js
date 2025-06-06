dd85843ff2a4a727c4fa63d571065bf497a5e9ab
gigi088789 <bot883327@gmail.com>
2025-06-06 17:19:19 -0400

    config.js

--- config.js
+++ config.js
@@ -34,7 +34,7 @@ MENTION_REPLY: process.env.MENTION_REPLY || "false",
 // make true if want auto voice reply if someone menetion you 
 MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/j97qag.mp4",
 // add custom menu and mention reply image url
-PREFIX: process.env.PREFIX || "🩸",
+PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
 BOT_NAME: process.env.BOT_NAME || "NLE🩸💤🧃",
 // add bot name here for menu
